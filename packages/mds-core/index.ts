c0bd631878328d5a9e4ad9ce2cc6d5820d33bbc2
Cedric Villa <cedric.villa@gmail.com>
2019-02-19 19:04:13 +0100

    export in index

--- packages/mds-core/index.ts
+++ packages/mds-core/index.ts
@@ -1,11 +1,5 @@
-// const MDSCore = {
-//     colors: require("./src/colors"),
-//     fonts: require("./src/fonts"),
-//     screenSizes: require("./src/screen-sizes"),
-// }
+import MDSColorScheme from "./src/colors"
+import MDSScreenSizes from "./src/screen-sizes"
+import MDSFonts from "./src/fonts"
 
-// export default MDSCore
-
-export const colors = import("./src/colors")
-export const fonts = require("./src/fonts")
-export const screenSizes = require("./src/screen-sizes")
+export { MDSColorScheme, MDSScreenSizes, MDSFonts }
