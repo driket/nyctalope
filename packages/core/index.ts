6311c397a5243f254843dfed7683bc6ea9089966
Cedric Villa <cedric.villa@gmail.com>
2019-02-20 21:17:22 +0100

    add system colors

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,3 +1,3 @@
-export { ColorScheme, ColorSchemeType } from "./src/colors"
+export { ColorScheme, ColorSchemeType, SystemColorsType } from "./src/colors"
 export { ScreenSizes, ScreenSizesType } from "./src/screen-sizes"
 export { Fonts, FontsType } from "./src/fonts"
--- packages/core/src/colors.ts
+++ packages/core/src/colors.ts
@@ -3,51 +3,65 @@
  * Should have both light and dark colors schemes
  */
 export type ThemedColorSchemeType = {
-    light: ColorSchemeType
-    dark: ColorSchemeType
+  light: ColorSchemeType
+  dark: ColorSchemeType
 }
 
 /**
  * Colors scheme type
  */
 export type ColorSchemeType = {
-    danger: string
-    success: string
-    main: string
-    inverted: string
-    background: string
-    grey: string
-    lightGrey: string
-    lighterGrey: string
-    highlight: string
+  danger: string
+  success: string
+  main: string
+  inverted: string
+  background: string
+  grey: string
+  lightGrey: string
+  lighterGrey: string
+  highlight: string
+  systemColors: SystemColorsType
+}
+
+export type SystemColorsType = {
+  red: string
+  purple: string
 }
 
 /**
  * Reference Color Scheme
  */
 export const ColorScheme: ThemedColorSchemeType = {
-    light: {
-        danger: "#D00020",
-        success: "#5EE89A",
-        main: "black",
-        inverted: "white",
-        background: "#FFFFFF",
-        grey: "#888",
-        lightGrey: "#BBB",
-        lighterGrey: "#EEE",
-        highlight: "rgb(12,95,254)",
+  light: {
+    danger: "#D00020",
+    success: "#5EE89A",
+    main: "black",
+    inverted: "white",
+    background: "#FFFFFF",
+    grey: "#888",
+    lightGrey: "#BBB",
+    lighterGrey: "#EEE",
+    highlight: "rgb(12,95,254)",
+    systemColors: {
+      red: "red",
+      purple: "purple",
     },
-    dark: {
-        danger: "#D00020",
-        success: "#2FA261",
-        main: "white",
-        inverted: "black",
-        background: "#1F2228",
-        grey: "#888",
-        lightGrey: "#555",
-        lighterGrey: "#333",
-        highlight: "rgb(42,125,254)",
+  },
+  dark: {
+    danger: "#D00020",
+    success: "#2FA261",
+    main: "white",
+    inverted: "black",
+    background: "#1F2228",
+    grey: "#888",
+    lightGrey: "#555",
+    lighterGrey: "#333",
+    highlight: "rgb(42,125,254)",
+    systemColors: {
+      red: "red",
+      purple: "purple",
     },
+  },
 }
 
 export default ColorScheme
