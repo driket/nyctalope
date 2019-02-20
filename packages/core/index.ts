fd321c129805a90a4945f178f09a8fb941bbc10d
Cedric Villa <cedric.villa@gmail.com>
2019-02-20 18:29:31 +0100

    move index

---
+++ packages/core/index.ts
@@ -0,0 +1,12 @@
+import ColorScheme, { ColorSchemeType } from "./src/colors"
+import ScreenSizes, { ScreenSizesType } from "./src/screen-sizes"
+import Fonts, { FontsType } from "./src/fonts"
+
+export {
+  ColorScheme,
+  ColorSchemeType,
+  ScreenSizes,
+  ScreenSizesType,
+  Fonts,
+  FontsType,
+}
