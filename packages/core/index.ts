912233e8dc32d0c75261c1254b47b7e169e577a4
Cedric Villa <cedric.villa@gmail.com>
2019-02-20 18:19:02 +0100

    move index to src

--- packages/core/index.ts
+++
@@ -1,5 +0,0 @@
-import ColorScheme from "./src/colors"
-import ScreenSizes from "./src/screen-sizes"
-import Fonts from "./src/fonts"
-
-export { ColorScheme, ScreenSizes, Fonts }
---
+++ packages/core/src/index.ts
@@ -0,0 +1,12 @@
+import ColorScheme, { ColorSchemeType } from "./colors"
+import ScreenSizes, { ScreenSizesType } from "./screen-sizes"
+import Fonts, { FontsType } from "./fonts"
+
+export {
+  ColorScheme,
+  ColorSchemeType,
+  ScreenSizes,
+  ScreenSizesType,
+  Fonts,
+  FontsType,
+}
