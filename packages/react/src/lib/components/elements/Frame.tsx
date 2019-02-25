5fd77f99565605e3fd09bd63e8a7480abd1c8b4b
Cedric Villa <cedric.villa@gmail.com>
2019-02-25 14:40:09 +0100

    Render children

--- packages/react/src/lib/components/elements/Frame.tsx
+++ packages/react/src/lib/components/elements/Frame.tsx
@@ -4,13 +4,17 @@ import { Component, ReactChildren, useContext } from 'react'
 import { ThemeContext } from '@nyctalope/core'
 import { jsx, css } from '@emotion/core'
 
-export const Frame = (props) => {
+type FrameProps = {
+  children: any
+}
+
+export const Frame = (props: FrameProps) => {
   const { colors } = useContext(ThemeContext)
   const frameStyle = css`
     background: ${colors.background};
     color: ${colors.main};
   `
-  return <div css={frameStyle}>Frame</div>
+  return <div css={frameStyle}>{props.children}</div>
 }
 
 export default Frame
