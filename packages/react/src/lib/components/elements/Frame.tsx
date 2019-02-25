4d7572a575ec42c11da5c23e3436597bbcea6788
Cedric Villa <cedric.villa@gmail.com>
2019-02-25 21:22:30 +0100

    use css-in-js instead of styled

--- packages/react/src/lib/components/elements/Frame.tsx
+++ packages/react/src/lib/components/elements/Frame.tsx
@@ -1,20 +1,19 @@
-/** @jsx jsx */
-/* eslint-disable import/first */
-import { Component, ReactChildren, useContext } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '@nyctalope/core'
-import { jsx, css } from '@emotion/core'
 
 type FrameProps = {
   children: any
+  style: any
 }
 
 export const Frame = (props: FrameProps) => {
   const { colors } = useContext(ThemeContext)
-  const frameStyle = css`
-    background: ${colors.background};
-    color: ${colors.main};
-  `
-  return <div css={frameStyle}>{props.children}</div>
+  const style = {
+    background: colors.background,
+    color: colors.main,
+    ...props.style,
+  }
+  return <div style={props.style}>{props.children}</div>
 }
 
 export default Frame
