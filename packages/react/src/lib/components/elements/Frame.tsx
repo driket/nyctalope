ea25b2e7a9205a2d6c23210d21bf114a3259a755
Cedric Villa <cedric.villa@gmail.com>
2019-02-26 20:04:33 +0100

    fix type and style

--- packages/react/src/lib/components/elements/Frame.tsx
+++ packages/react/src/lib/components/elements/Frame.tsx
@@ -2,18 +2,19 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '@nyctalope/core'
 
 type FrameProps = {
-  children: any
-  style: any
+  children?: any
+  style?: any
 }
 
 export const Frame = (props: FrameProps) => {
   const { colors } = useContext(ThemeContext)
-  const style = {
+  const { style = {} } = props
+  const combinedStyle = {
     background: colors.background,
     color: colors.main,
-    ...props.style,
+    ...style,
   }
-  return <div style={props.style}>{props.children}</div>
+  return <div style={combinedStyle}>{props.children}</div>
 }
 
 export default Frame
