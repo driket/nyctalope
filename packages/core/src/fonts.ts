5e21e412bbd983c733724a5283f992cf0bb97d77
Cedric Villa <cedric.villa@gmail.com>
2019-03-02 18:30:40 +0100

    fixed semicolon in fonts

--- packages/core/src/fonts.ts
+++ packages/core/src/fonts.ts
@@ -1,14 +1,14 @@
 export type FontsType = {
-    main: string
-    heading: string
-    brand: string
+  main: string
+  heading: string
+  brand: string
 }
 
 export const Fonts: FontsType = {
-    main:
-        "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;",
-    brand: "'Comfortaa', cursive;",
-    heading: "-apple-system, 'Helvetica Neue'",
+  main:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+  brand: "'Comfortaa', cursive",
+  heading: "-apple-system, 'Helvetica Neue'",
 }
 
 export default Fonts
