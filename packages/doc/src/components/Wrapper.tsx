2f0d366ab1cafcdb08a300e770ee12d3f50d100b
Cedric Villa <cedric.villa@gmail.com>
2019-03-03 12:22:00 +0100

    remove styled + add live editing

--- packages/doc/src/components/Wrapper.tsx
+++ packages/doc/src/components/Wrapper.tsx
@@ -1,8 +1,11 @@
-/** @jsx jsx */
-import React, { useContext } from 'react'
-import { darken, transparentize } from 'polished'
-import { jsx, css, SerializedStyles } from '@emotion/core'
+import React, { CSSProperties, useContext, SFC, ReactChildren } from 'react'
+import { darken, transparentize, mix } from 'polished'
 import { ColorScheme, Fonts, ThemeContext } from '@nyctalope/core'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import reactElementToJSXString from 'react-element-to-jsx-string'
+import * as NyctalopeComponents from '@nyctalope/react'
+import * as NyctalopeDocComponents from '../components'
+
 type WrapperProps = {
   colorSchemeSeparatorDirection: 'vertical' | 'horizontal'
   minHeight: string
@@ -11,118 +14,146 @@ type WrapperProps = {
   children: any
 }
 
-export const Wrapper = (props: WrapperProps) => {
+export const Wrapper = (props) => {
   const { colorSchemeSeparatorDirection = 'vertical' } = props
   const { minHeight } = props
   const currentColorScheme = useContext(ThemeContext)
+  const { colors } = currentColorScheme
   const colorSchemes = props.bothColorSchemes
     ? ['dark', 'light']
     : [currentColorScheme.prefersColorScheme]
-  const styleHeight = minHeight
-    ? css`
-        min-height: ${minHeight};
-      `
-    : css``
-  const styleWidth =
-    colorSchemeSeparatorDirection == 'horizontal'
-      ? css`
-          width: 50%;
-          &:first-child {
-            margin-right: 20px;
-          }
-        `
-      : css`
-          width: 100%;
-        `
-  const style = [styleWidth, styleHeight]
+  const wrapperStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'start',
+  }
+  const code = reactElementToJSXString(props.children)
+  // const code =
+  // "<Stack distribute='start'>\n    {['default', 'primary', 'secondary', 'ghost', 'danger'].map((type) => (\n      <Button type={type}>\n        {type.charAt(0).toUpperCase() + type.slice(1)}\n      </Button>\n    ))}\n  </Stack>\n  "
+  const canvasStyle: CSSProperties = {
+    minHeight: minHeight ? minHeight : 'initial',
+    width:
+      colorSchemeSeparatorDirection == 'horizontal'
+        ? 'calc(100% - 30px)'
+        : 'calc(100% - 30px)',
+    // marginLeft: index == 1 ? '20px' : '0px',
+  }
+  const checkerBoardStyle: CSSProperties = {}
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: row;
-      `}
-    >
-      {colorSchemes.map((theme) => {
-        const wrapperTheme = {
-          prefersColorScheme: theme,
-          colors: ColorScheme[theme],
-          fonts: Fonts,
-        }
-        console.log('wrapperTheme:', wrapperTheme)
-        return (
-          <ThemeContext.Provider value={wrapperTheme}>
-            <StoryRenderBackground darkMode={theme} css={style} {...props}>
-              {props.children}
-            </StoryRenderBackground>
-          </ThemeContext.Provider>
-        )
-      })}
+    <div style={wrapperStyle}>
+      <LiveProvider
+        code={code}
+        scope={{ ...NyctalopeComponents, ...NyctalopeDocComponents }}
+        style={{ width: '100%' }}
+      >
+        <Canvas {...props} style={canvasStyle}>
+          {colorSchemes.map((theme, index) => {
+            const wrapperTheme = {
+              prefersColorScheme: theme,
+              colors: ColorScheme[theme],
+              fonts: Fonts,
+            }
+            return (
+              <ThemeContext.Provider value={wrapperTheme}>
+                <CheckerBoard {...props}>
+                  <LivePreview />
+                </CheckerBoard>
+              </ThemeContext.Provider>
+            )
+          })}
+          <LiveEditor
+            style={{
+              backgroundColor: mix(0.97, colors.background, colors.main),
+              color: colors.main,
+            }}
+          />
+          <LiveError />
+        </Canvas>
+      </LiveProvider>
     </div>
   )
 }
 
-const StoryRenderBackground = (props) => {
+type CanvasProps = {
+  style: CSSProperties
+  children: any
+  checkerBoard: boolean
+}
+
+const Canvas = (props: CanvasProps) => {
   const { colors } = useContext(ThemeContext)
-  return (
-    <div
-      css={css`
-        border: 1px solid ${colors.lighterGrey};
-        padding: 15px;
-      `}
-      {...props}
-    >
-      <CheckerBoard {...props}>{props.children}</CheckerBoard>
-    </div>
-  )
+  const canvasBGStyle = {
+    border: `1px solid ${colors.lighterGrey}`,
+    padding: '15px',
+    ...props.style,
+  }
+  return <div style={canvasBGStyle}>{props.children}</div>
 }
 
 const CheckerBoard = (props) => {
   const { colors } = useContext(ThemeContext)
-  const { themedBackgroundColor = 'background' } = props
   const { checkerBoard = false } = props
-  const backgroundColor = colors[themedBackgroundColor]
+  const backgroundColor = colors.background
   const alternateBackgroundColor = darken(0.05, backgroundColor)
-  const baseBackgroundStyle = css`
-    padding: 10px;
-    background-color: ${backgroundColor};
-    color: ${colors.main};
-    height: calc(100% - 20px);
-  `
-  const checkerBackgroundStyle = checkerBoard
-    ? css`
-        padding: 10px;
-        background-color: ${backgroundColor};
-        background-image: -moz-linear-gradient(
-            45deg,
-            ${alternateBackgroundColor} 25%,
-            transparent 25%
-          ),
-          -moz-linear-gradient(-45deg, ${alternateBackgroundColor} 25%, transparent
-                25%),
-          -moz-linear-gradient(45deg, transparent 75%, ${alternateBackgroundColor}
-                75%),
-          -moz-linear-gradient(-45deg, transparent 75%, ${alternateBackgroundColor}
-                75%);
-        background-image: -webkit-gradient(
-            linear,
-            0 100%,
-            100% 0,
-            color-stop(0.25, ${alternateBackgroundColor}),
-            color-stop(0.25, transparent)
-          ),
-          -webkit-gradient(linear, 0 0, 100% 100%, color-stop(0.25, ${alternateBackgroundColor}), color-stop(0.25, transparent)),
-          -webkit-gradient(linear, 0 100%, 100% 0, color-stop(0.75, transparent), color-stop(0.75, ${alternateBackgroundColor})),
-          -webkit-gradient(linear, 0 0, 100% 100%, color-stop(0.75, transparent), color-stop(0.75, ${alternateBackgroundColor}));
-        -moz-background-size: 20px 20px;
-        background-size: 20px 20px;
-        -webkit-background-size: 20px 21px; /* override value for shitty webkit */
-        background-position: 0 0, 10px 0, 10px -10px, 0px 10px;
-        color: ${colors.main};
-      `
-    : css``
-  const backgroundStyle = [baseBackgroundStyle, checkerBackgroundStyle]
+  const baseBackgroundStyle = {
+    padding: '10px',
+    backgroundColor: backgroundColor,
+    color: colors.main,
+    height: 'calc(100% - 20px)',
+  }
+  const mozCheckerBackgroundStyle: CSSProperties = {
+    backgroundImage: `
+      -moz-linear-gradient(
+        45deg,
+        ${alternateBackgroundColor} 25%,
+        transparent 25%
+      ),
+      -moz-linear-gradient(
+        -45deg,
+        ${alternateBackgroundColor} 25%,
+        transparent 25%
+      ),
+      -moz-linear-gradient(
+        45deg,
+        transparent 75%,
+        ${alternateBackgroundColor} 75%
+      ),
+      -moz-linear-gradient(
+        -45deg,
+        transparent 75%,
+        ${alternateBackgroundColor} 75%
+      )
+      `,
+  }
+  const webkitCheckerBackgroundStyle: CSSProperties = {
+    backgroundImage: `
+      -webkit-gradient(
+        linear,
+        0 100%,
+        100% 0,
+        color-stop(0.25, ${alternateBackgroundColor}),
+        color-stop(0.25, transparent)
+      ),
+      -webkit-gradient(linear, 0 0, 100% 100%, color-stop(0.25, ${alternateBackgroundColor}), color-stop(0.25, transparent)),
+      -webkit-gradient(linear, 0 100%, 100% 0, color-stop(0.75, transparent), color-stop(0.75, ${alternateBackgroundColor})),
+      -webkit-gradient(linear, 0 0, 100% 100%, color-stop(0.75, transparent), color-stop(0.75, ${alternateBackgroundColor}))
+    `,
+  }
 
-  return <div css={[backgroundStyle]}>{props.children}</div>
+  const checkerBackgroundStyle: CSSProperties = {
+    backgroundColor: backgroundColor,
+    ...(checkerBoard ? mozCheckerBackgroundStyle : {}),
+    ...(checkerBoard ? webkitCheckerBackgroundStyle : {}),
+    backgroundSize: '20px 20px',
+    color: colors.main,
+    backgroundPosition: '0 0, 10px 0, 10px -10px, 0px 10px',
+  }
+
+  const backgroundStyle = {
+    ...baseBackgroundStyle,
+    ...checkerBackgroundStyle,
+  }
+  return <div style={backgroundStyle}>{props.children}</div>
 }
 
 export default Wrapper
