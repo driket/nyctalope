2e55ad8eab2475299f8740a0244134d0f6b98912
Cedric Villa <cedric.villa@gmail.com>
2019-04-04 19:26:06 +0200

    semicolon

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -4,18 +4,22 @@
 /* eslint-disable import/first */
 
 // Layouts
-export { Grid } from './src/lib/components/layout/Grid'
-export { Stack } from './src/lib/components/layout/Stack'
-export { Frame } from './src/lib/components/layout/Frame'
+export { Grid } from './src/lib/components/layout/Grid';
+export { Stack } from './src/lib/components/layout/Stack';
+export { Frame } from './src/lib/components/layout/Frame';
 // Documents
-export { WebView } from './src/lib/components/document/WebView'
+export { WebView } from './src/lib/components/document/WebView';
 // Elements
-export { Button } from './src/lib/components/elements/Button'
-export { Icon, FeatherIcon } from './src/lib/components/elements/Icon'
+export { Button } from './src/lib/components/elements/Button';
+export { Icon, FeatherIcon } from './src/lib/components/elements/Icon';
 // Data Viz
-export { Pill, PillSegment } from './src/lib/components/data-viz/Pill'
+export {
+  Pill,
+  PillSegment,
+  PillStyle,
+} from './src/lib/components/data-viz/Pill';
 
 /**
  * Hooks
  */
-export { useDarkMode } from './src/lib/hooks/use-darkmode'
+export { useDarkMode } from './src/lib/hooks/use-darkmode';
